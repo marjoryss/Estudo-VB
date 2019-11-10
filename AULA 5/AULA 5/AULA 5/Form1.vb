2dac67f1407088745d10759207df0f6ae8cc810b
marjoryss <marjory.savanini2@gmail.com>
2019-11-10 11:50:24 -0300

    Update Form1.vb
    aula 7 implementacao do botao ok e sair

--- AULA 5/AULA 5/AULA 5/Form1.vb
+++ AULA 5/AULA 5/AULA 5/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        'Mostra uma caixa de mensagem para o usuario
+        MessageBox.Show("Ola, " & txtName.Text & " Bem vindo ao Visual Basic 2015", "Mensagem ao usuario")
 
+    End Sub
+
+    Private Sub btnSair_Click(sender As Object, e As EventArgs) Handles btnSair.Click
+        'Fim do programa
+        Me.Close()
+    End Sub
 End Class
