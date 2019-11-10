01e41b086b267c57425981b251e3dff67a010431
marjoryss <marjory.savanini2@gmail.com>
2019-11-10 11:27:55 -0300

    Update Form1.Designer.vb
    aula 6 criacao de botoes e caixa de texto

--- AULA 5/AULA 5/AULA 5/Form1.Designer.vb
+++ AULA 5/AULA 5/AULA 5/Form1.Designer.vb
@@ -22,17 +22,64 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnSair = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(55, 39)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(177, 25)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Coloque seu nome"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(60, 85)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(281, 29)
+        Me.txtName.TabIndex = 1
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(60, 295)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(159, 50)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "&ok"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnSair
+        '
+        Me.btnSair.Location = New System.Drawing.Point(329, 295)
+        Me.btnSair.Name = "btnSair"
+        Me.btnSair.Size = New System.Drawing.Size(159, 50)
+        Me.btnSair.TabIndex = 3
+        Me.btnSair.Text = "s&air"
+        Me.btnSair.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(608, 416)
+        Me.Controls.Add(Me.btnSair)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
         Me.Name = "Form1"
         Me.Text = "Hello World"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnSair As Button
 End Class
